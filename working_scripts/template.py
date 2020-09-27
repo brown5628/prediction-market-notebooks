82e85fcf5b8ff5a9193e8ec1812bdfe0654f8c87
brown5628 <brown5628@gmail.com>
2020-09-27 11:32:15 -0400

    add template

---
+++ working_scripts/template.py
@@ -0,0 +1,40 @@
+# %% [markdown]
+# # Question Text
+# ## Reference
+# - Prediction Market:
+# - Link:
+# - Close Date:
+#
+# ## Initial Prediction
+# *updated 2020-09-27*
+# 
+# ### Thought Process
+# Describe relevant sub questions and their reference classes. 
+# Describe relevant other perspectives.
+#
+# ### Model(s)
+# %%
+1 + 1
+
+# %% [markdown]
+# ### Prediction
+# Transcribe to format of the market. Inside view adjustment and ensemble.
+
+# ## First Update
+# # *updated 2020-09-27*
+# 
+# ### Thought Process
+# Describe relevant sub questions and their reference classes. 
+# Describe relevant other perspectives.
+#
+# ### Model(s)
+# %%
+1 + 1
+
+# %% [markdown]
+# ### Prediction
+# Transcribe to format of the market. Inside view adjustment and ensemble.
+
+# %% [markdown]
+# ## Post-mortem
+# 5 whys?
